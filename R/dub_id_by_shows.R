e09f3779093f9c5feac651289770ba2932f0f742
szigony <szigeti.patrik@gmail.com>
2019-05-12 12:29:14 +0200

    A helper function to filter for texts or characters of a specific show.

---
+++ R/dub_id_by_shows.R
@@ -0,0 +1,24 @@
+#' IDs for shows
+#'
+#' Get the list of IDs that are in scope for the selected set of shows.
+#'
+#' This would tipically be used to filter for texts or characters of a specific show.
+#'
+#' @export
+#'
+#' @param shows A show or a list of shows for which we'd like to return the \code{dub_id}s.
+#'
+#' @format Returns a list of \code{dub_id}s.
+#'
+#' @examples
+#' dub_id_by_shows("Fifth Gear")
+#' dub_id_by_shows(c("Fifth Gear", "Finding Bigfoot"))
+#'
+#' @seealso dub_shows, dub_metadata
+
+dub_id_by_shows <- function(shows) {
+  in_scope_dubs <- dub_data()$dub_metadata %>%
+    filter(show %in% shows)
+
+  return(in_scope_dubs$dub_id)
+}
